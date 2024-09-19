1360495c3143b6044004120fd48fe9642f696249
samiullahmamu <samiullahudoy@gmail.com>
2024-09-19 18:35:53 +0600

    add money

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log('home)added')
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+
+    document.getElementById('input-add-money').value;
+
+    const pinNumberInput =document.getElementById('input-pin-num').value;
+     
+    console.log('addmoney,pin num');
+})
