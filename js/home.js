77392faffe532bd015a78628e098c6af46b96e89
samiullahmamu <samiullahudoy@gmail.com>
2024-09-19 19:59:51 +0600

    home js

--- js/home.js
+++ js/home.js
@@ -1,9 +1,20 @@
-document.getElementById('btn-add-money').addEventListener('click',function(event){
-    event.preventDefault();
+document.getElementById('btn-add-money').addEventListener('click',function(){
+    Event.preventDefault();
+
 
     document.getElementById('input-add-money').value;
 
     const pinNumberInput =document.getElementById('input-pin-num').value;
      
-    console.log('addmoney,pin num');
+    if(pinNumberInput ==='1234'){
+    const addMoneyNumber = parseFloat(addMoneyInput);
+    const balanceNumber =parseFloat(balance);
+    const newBalance = addMoneyNumber +balanceNumber;
+      
+    document.getElementById('balance').innerText =newBalance;
+
+        }
+    else(
+        alert('Faild to add money! please try again.')
+    )
 })
